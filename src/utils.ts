4b8b179ecd43a6394acfbd6d9ca6ebb3a2e069c3
Peter Todd <ptr.tdd@gmail.com>
2024-08-23 15:50:49 +0100

    minor cleanup

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,4 @@
-/**
- * @param {Array<[number, number]>} args
- */
-export function filterPoly(args: [number, number][], xData: Float32Array, yData: Float32Array) {
-    const points = args;
+export function filterPoly(points: [number, number][], xData: Float32Array, yData: Float32Array) {
     let minX = Number.MAX_VALUE;
     let minY = Number.MAX_VALUE;
     let maxX = Number.MIN_VALUE;
@@ -13,22 +9,19 @@ export function filterPoly(args: [number, number][], xData: Float32Array, yData:
         minY = Math.min(minY, pt[1]);
         maxY = Math.max(maxY, pt[1]);
     }
-    const data1 = xData;
-    const data2 = yData;
-    const vs = points;
 
     const predicate = (i: number) => {
-        const x = data1[i];
-        const y = data2[i];
+        const x = xData[i];
+        const y = yData[i];
         let inside = false;
         if (x < minX || x > maxX || y < minY || y > maxY || Number.isNaN(x) || Number.isNaN(y)) {
             return false;
         }
-        for (let i = 0, j = vs.length - 1; i < vs.length; j = i++) {
-            const xi = vs[i][0];
-            const yi = vs[i][1];
-            const xj = vs[j][0];
-            const yj = vs[j][1];
+        for (let i = 0, j = points.length - 1; i < points.length; j = i++) {
+            const xi = points[i][0];
+            const yi = points[i][1];
+            const xj = points[j][0];
+            const yj = points[j][1];
 
             const intersect = ((yi > y) !== (yj > y))
                 && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
@@ -37,5 +30,5 @@ export function filterPoly(args: [number, number][], xData: Float32Array, yData:
         return inside;
     }
 
-    return new Uint32Array(data1.length).map((_, i) => i).filter(predicate);
+    return new Uint32Array(xData.length).map((_, i) => i).filter(predicate);
 }
